03db478fafc07fab5871f31f44f5dabaabc9e2c8
wbvguo <vinkwok@outlook.com>
2024-12-27 09:33:52 -0800

    update

--- R/dir.R
+++ R/dir.R
@@ -1,10 +1,10 @@
-#' `mkdir`
-#'
 #' create a directory if it does not exist
 #'
-#' @param dir a character string of directory path
+#' This function checks whether a directory exists at the specified path. If the directory does not exist, it is created.
 #'
-#' @return a directory created if it does not exist
+#' @param dir A character string specifying the path of the directory to be checked and created if necessary.
+#'
+#' @return The specified directory path, with the directory created if it did not exist.
 #' @export
 #'
 #' @examples
--- R/save.R
+++ R/save.R
@@ -1,21 +1,23 @@
-#' `save2pdf`
+#' save a plot to a PDF file
 #'
-#' save a plot to a pdf file
+#' This function saves a plot to a PDF file. It can create a new PDF or append the plot to an existing file. If the file already exists and `overwrite` is set to `FALSE`, a new uniquely named file is created.
 #'
-#' @param file a character string of the file path
-#' @param plot_code a language object of the plot code, usually use `quote` to wrap the code
-#' @param overwrite a logical value, whether to overwrite the existing file, default is FALSE
-#' @param append a logical value, whether to append the new plot to the existing file, default is TRUE
-#' @param ... other parameters passed to `pdf`
+#' @param file A character string specifying the file path for the PDF.
+#' @param plot_code A language object representing the plot code to be executed. Typically, use `quote` to wrap the plot code.
+#' @param overwrite Logical. If `TRUE`, overwrites the existing file. Default is `FALSE`.
+#' @param append Logical. If `TRUE`, appends the new plot to the existing file by combining the old and new PDFs. Default is `TRUE`.
+#' @param ... Additional arguments passed to the `pdf` function, such as `width` or `height`.
 #'
-#' @return a pdf file
+#' @return A PDF file is created at the specified location.
 #' @export
 #'
-#' @examples save2pdf(file = "mytest_plot.pdf", width = 4, height = 4, overwrite = FALSE,
-#'                    plot_code = quote({
-#'                      t = seq(0, 100, 1)
-#'                      plot(cos(t) + t*sin(t), sin(t) - t* cos(t), type = "l")
-#'                    }))
+#' @examples
+#' # Save a simple plot to a PDF
+#' save2pdf(file = "mytest_plot.pdf", width = 4, height = 4, overwrite = FALSE,
+#'          plot_code = quote({
+#'            t = seq(0, 100, 1)
+#'            plot(cos(t) + t*sin(t), sin(t) - t*cos(t), type = "l")
+#'          }))
 save2pdf = function(file=NA, plot_code=NA, overwrite=FALSE, append=TRUE, ...){
   if(is.na(file)){stop("please specify the files to save the plot")}
   if(!is.language(plot_code)){stop("please specify the code to plot")}
